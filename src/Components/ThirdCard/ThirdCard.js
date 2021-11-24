172eeb339bd9db3a84c7a5802fbe17d309e1dfdf
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-11-24 10:53:53 +0530

    Cards

--- src/Components/ThirdCard/ThirdCard.js
+++ src/Components/ThirdCard/ThirdCard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './ThirdCard.module.css'
+import partners from '../../assets/Group 13.png'
 
 const ThirdCard = () => {
     return (
         <div className={styles.parent}>
-            
+            <h1>Arcade Partners</h1>
+            <p>all platforms like iOS, Android and also on browsers like chrome, Safari and so on.all platforms like iOS, Android and also on browsers like chrome, Safari and so on.all platforms like iOS, Android and also on browsers like chrome, Safari and so on.all platforms like iOS, Android and also on browsers like chrome, Safari and so onall platforms like iOS, Android and also on browsers like chrome, Safari and so on.all platforms like iOS, Android and also on browsers like chrome, Safari and so on.all platforms like iOS, Android and also on browsers like chrome, Safari and so on.all platforms like iOS, Android and also on browsers like chrome, Safari and so on..</p>
+            <img src={partners} />
         </div>
     )
 }
