829a7bee96667818f1bdd7bda43da009aa153b7c
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-12-07 22:13:01 +0530

    Final

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import styles from './Navbar.module.css'
 import search from '../../assets/search.png'
+import air from '../../assets/Group 18.png'
 
 const Navbar = () => {
     return (
-        <div className={styles.parent}>
+        <div className={styles.parent1}>
             <div className={styles.flex}>
-                <p className={styles.text}>Home</p>
-                <p className={styles.text}>Air Cloud</p>
-                <p className={styles.text}>Platforms</p>
-                <p className={styles.text}>About Us</p>
+                <p className={styles.textNav}>Home</p>
+                <p className={styles.textNav}>Air Cloud</p>
+                <p className={styles.textNav}>Platforms</p>
+                <p className={styles.textNav}>About Us</p>
                 <img src={search} alt='search' className={styles.icon} />
             </div>
-            <div className={styles.smallerParent}>
-                <button className={styles.b1}><img src={search} alt="" /></button>
-            </div>
+            <button className={styles.b1}><img className={styles.airBtn} src={air} alt="" /></button>
+
         </div>
     )
 }
